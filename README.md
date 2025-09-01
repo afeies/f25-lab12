94c8c7b94e7ca483ddec02273098f110f2272773
Hammad Ahmad <hammada@umich.edu>
2025-09-01 11:19:22 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Fall 2025 Lab 12: APIs
+
+See the handout at [https://github.com/CMU-17-214/f2025/blob/main/labs/lab12.md].
---
+++ src/main/java/edu/cmu/Library/LibraryAccount.java
@@ -0,0 +1,21 @@
+package edu.cmu.Library;
+
+public class LibraryAccount {
+    private LibraryService libraryService;
+ 
+    /**
+     * Retrieves an array of checked out books associated with the specified user ID. If the user
+     * has no books checked out, the returned list will be empty. Since multiple households may
+     * share a single account, the user ID is of the form "libraryID:userName".
+     * e.g., "12345:John Doe"
+     *
+     * @param userId the ID of the user whose books are to be retrieved
+     * @return an array of Book objects the user has checked out
+     */
+    public Book[] getBooks(String userId) {
+        String[] parts = userId.split(":");
+        String name = parts[0];
+        String id = parts[1];
+        return libraryService.getBooks(name, id);        
+    }
+}
